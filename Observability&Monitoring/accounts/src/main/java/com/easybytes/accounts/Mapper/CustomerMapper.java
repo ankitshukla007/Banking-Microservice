cc43dc4046cfcf728712db5ffc3e13f89f8eb34d
ankitshukla007 <shuklaankit59430900@gmail.com>
2024-08-10 00:42:21 +0530

    Observability&Monitoring with logs(loki, alloy and grafana), metrics(Micrometer, Prometheus, grafana) and traces(openTelemetry, tempo and Grafana))

---
+++ Observability&Monitoring/accounts/src/main/java/com/easybytes/accounts/Mapper/CustomerMapper.java
@@ -0,0 +1,29 @@
+package com.easybytes.accounts.Mapper;
+
+import com.easybytes.accounts.dto.CustomerDetailsDto;
+import com.easybytes.accounts.dto.CustomerDto;
+import com.easybytes.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobilenumber(customer.getMobilenumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobilenumber(customerDto.getMobilenumber());
+        return customer;
+    }
+
+    public static CustomerDetailsDto mapToCustomerDetailsDto(Customer customer, CustomerDetailsDto customerDetailsDto) {
+        customerDetailsDto.setName(customer.getName());
+        customerDetailsDto.setEmail(customer.getEmail());
+        customerDetailsDto.setMobilenumber(customer.getMobilenumber());
+        return customerDetailsDto;
+    }
+}
